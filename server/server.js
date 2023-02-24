28ff779cd38711a9244e3f9cb7a512cdfd269485
cristianh <crishernandez08@hotmail.com>
2023-02-24 11:32:01 -0500

    UPDATE: habilitando cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
-
+const cors = require('cors')
 const path = require('path');
 
 const app = express();
 let server = http.createServer(app);
+
+// enable local connections - Cors
+let options = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false
+}
+
+
+//this.app.options('*', cors())//EL '*' INDICA QUE SE ACEPTAN TODAS LAS CONEXIONES DE CUALQUIER SERVIDOR.
+
+
+app.use(cors(options));//INDICAMOS A EXPRESS QUE UTILICE LOS CORS.
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
