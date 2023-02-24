f117099c31b74b28b361232b94454c4392563d61
cristianh <crishernandez08@hotmail.com>
2023-02-24 12:42:37 -0500

    PRUEBA

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", ["*","localhost"]);
+    res.header("Access-Control-Allow-Origin", ["*","localhost","https://localhost"]);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
