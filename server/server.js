87c8aeaf9419f97b59c883a8f8b5bb3137f1d9c8
cristianh <crishernandez08@hotmail.com>
2023-02-24 12:10:33 -0500

    PRUEBA

--- server/server.js
+++ server/server.js
@@ -5,12 +5,29 @@ const cors = require('cors')
 const path = require('path');
 
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
 let server = http.createServer(app, {
     cors: {
         origin: "*"
     }
 });
 
+// enable local connections - Cors
+let options = {
+    "origin": "*"
+}
+
+
+
+app.use(cors(options));//INDICAMOS A EXPRESS QUE UTILICE LOS CORS.
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
